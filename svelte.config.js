2bebf2fc502721891ed35f0f3805b8908030bbeb
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2023-05-31 11:10:34 +0530

    build commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-static"
+const dev = process.argv.includes("dev")
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,14 +9,12 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: "build",
 			assets: "build",
-			fallback: index.html,
-			// precompress: false,
-			// strict: true,
 		}),
+		paths: {
+			base: dev ? "" : process.env.BASE_PATH,
+		},
 	},
 }
 
